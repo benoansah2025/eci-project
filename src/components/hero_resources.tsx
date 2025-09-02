6c87eeee8959111bf1bec91556c3847839ec986e
benoansah2025 <memphisperla@gmail.com>
2025-09-02 21:08:03 +0000

    fix: Worked on the layout of resources tab as well as introduce a hero image to ensure the page looks visually compelling

--- src/components/hero_resources.tsx
+++ src/components/hero_resources.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/about_2.jpg";
+import heroImage from "@/assets/resource_library.jpg";
 
 const Hero = () => {
   return (
@@ -18,12 +18,12 @@ const Hero = () => {
       <div className="relative z-10 container mx-auto text-center md:text-left">
         <div className="max-w-4xl mx-auto text-center ">
           <h1 className="md:text-7xl font-bold text-primary mb-8 pt-40">
-            About ECI
+            Resource Library
             {/* <span className="text-primary block mt-3">Employment Success</span> */}
           </h1>
           
           <p className="text-lg md:text-3xl text-primary  mb-8 max-w-1xl pt-15">
-            Founded with a passion for empowering Ghana's youth, we bridge the gap between education and employment, creating pathways for young professionals to thrive in the modern economy.
+            Access our comprehensive collection of career development resources to accelerate your professional journey
           </p>
           
         </div>
